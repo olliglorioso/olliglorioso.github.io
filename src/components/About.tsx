9960c082f9548e377de572229137ceaf28bd5ea3
Olli Glorioso <olli.glorioso@avanio.com>
2022-06-20 21:09:15 +0000

    Now the visitor can play with the head :)

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,20 +1,77 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-// import { Environment } from '@react-three/drei'
-
+import { Physics, usePlane } from '@react-three/cannon'
 import Model from './Fromblender'
 
+const Plane = () => {
+   const [ref] = usePlane(() => ({
+      rotation: [ -Math.PI / 2, 0, 0],
+      position: [0, -4.8, 0]
+   }))
+
+   // Create planes to represent the ground and the ceiling
+   const ground = usePlane(() => ({
+      position: [0, -10, 0],
+      rotation: [ -Math.PI / 2, 0, 0],
+      mass: 0,
+      args: [20, 20, 20],
+      material: 'ground',
+      receiveShadow: true
+   }))
+   const ceiling = usePlane(() => ({
+      position: [0, 5, 0],
+      rotation: [ Math.PI / 2, 0, 0],
+      mass: 0,
+      args: [20, 20, 20],
+      material: 'ceiling',
+      receiveShadow: true
+   }))
+   const leftSide = usePlane(() => ({
+      position: [-4, 0, 0],
+      rotation: [0, Math.PI / 2, 0],
+      mass: 0,
+      args: [20, 20, 20],
+      material: 'leftSide',
+      receiveShadow: true
+   }))
+   const rightSide = usePlane(() => ({
+      position: [4, 0, 0],
+      rotation: [0, -Math.PI / 2, 0],
+      mass: 0,
+      args: [20, 20, 20],
+      material: 'rightSide',
+      receiveShadow: true
+   }))
+
+
+   return (
+      <mesh rotation={[ -Math.PI / 2, 0, 0]} position={[0, -3, 0]} >
+         {/* <planeBufferGeometry attach="geometry" args={[ 100, 100 ]} /> */}
+         <meshStandardMaterial attach="material" color="#5C4033" />
+      </mesh>
+   )
+}
+
+
 export default function App() {
+  
   return (
     <div style={{ height: "100vh", width: "100vw", marginTop: 10, marginLeft: "auto", marginRight: 10}}>
-      <Canvas camera={{ fov: 33, zoom: 1.1, near: 1, far: 1000 }}>
-         <color attach="background" args={["#f0ede1"]} />
-         <directionalLight position={[10, 10, 5]} intensity={2} />
-         <directionalLight position={[-10, -10, -5]} intensity={1} />
-        <Suspense fallback={null}>
-          <Model/>
-          {/* <Environment background /> */}
-        </Suspense>
+      <Canvas shadows>
+         <directionalLight 
+            position={[10, 10, 5]} 
+            intensity={2} 
+         />
+         <directionalLight 
+            position={[-10, -10, -5]} 
+            intensity={1}
+            castShadow 
+         />
+         <Physics>
+            <Plane />
+            <Model />
+         </Physics>
+         
       </Canvas>
     </div>
   )
