240199207c68dfabf825497c77bb0905f878e761
Olli Glorioso <olli.glorioso@avanio.com>
2022-07-30 21:51:34 +0300

    Google console configs & resume & text to left corner [build]

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AnimateKeyframes } from "react-simple-animate"
 import { socials } from "./constants"
 const style = { height: 150, width: 150 }
 const mR = { marginRight: 9 }
+const props = { play: true, duration: 2, iterationCount: "infinite" }
 const mL = { marginLeft: 9 }
 
 function App() {
@@ -12,85 +13,89 @@ function App() {
 	const [play2, setPlay2] = useState(false)
 	const [play3, setPlay3] = useState(false)
 	const [play4, setPlay4] = useState(false)
-	const props = { play: true, duration: 2, iterationCount: "infinite" }
-
+	
 	return (
-		<div className='container'>
-			<div className='container_row'>
-				<div style={mR}>
-					<AnimateKeyframes
-						{...props}
-						pause={!play1}
-						keyframes={[
-							"transform: translateX(0px)",
-							"transform: translateX(-200px)",
-							"transform: translate(-200px, -200px)",
-							"transform: translate(0, -200px)",
-							"transform: translate(0)"
-						]}
-					>
-						<SocialIcon url={socials[0]} 
-							style={style} 
-							onMouseOver={() => setPlay1(true)} 
-							onMouseOut={() => setPlay1(false)} 
-						/>
-					</AnimateKeyframes>
+		<div>
+			<div className="name_title">Olli Glorioso</div>
+			<div className="text">Software engineering & data science.</div>
+			<div><a className="text" style={{ borderBottom: "1px solid black" }} href={`${process.env.PUBLIC_URL}/pdf-open-parameters.pdf`} download="olliglorioso_resume" >Download resume</a></div>
+			<div className='container'>
+				<div className='container_row'>
+					<div style={mR}>
+						<AnimateKeyframes
+							{...props}
+							pause={!play1}
+							keyframes={[
+								"transform: translateX(0px)",
+								"transform: translateX(-200px)",
+								"transform: translate(-200px, -200px)",
+								"transform: translate(0, -200px)",
+								"transform: translate(0)"
+							]}
+						>
+							<SocialIcon url={socials[0]} 
+								style={style} 
+								onMouseOver={() => setPlay1(true)} 
+								onMouseOut={() => setPlay1(false)} 
+							/>
+						</AnimateKeyframes>
+					</div>
+					<div style={mL}>
+						<AnimateKeyframes
+							{...props}
+							pause={!play2}
+							keyframes={[
+								"opacity: 1",
+								"opacity: 0",
+								"opacity: 1"
+							]}
+						>
+							<SocialIcon 
+								url={socials[1]} 
+								style={style} 
+								onMouseOver={() => setPlay2(true)} 
+								onMouseOut={() => setPlay2(false)} 
+							/>
+						</AnimateKeyframes>
+					</div>
 				</div>
-				<div style={mL}>
-					<AnimateKeyframes
-						{...props}
-						pause={!play2}
-						keyframes={[
-							"opacity: 1",
-							"opacity: 0",
-							"opacity: 1"
-						]}
-					>
-						<SocialIcon 
-							url={socials[1]} 
-							style={style} 
-							onMouseOver={() => setPlay2(true)} 
-							onMouseOut={() => setPlay2(false)} 
-						/>
-					</AnimateKeyframes>
-				</div>
-			</div>
-			<br />
-			<div className='container_row'>
-				<div style={mR}>
-					<AnimateKeyframes
-						{...props}
-						pause={!play3}
-						keyframes={[
-							"transform: rotate(0deg)",
-							"transform: rotate(360deg)",
-						]}
-					>
-						<SocialIcon 
-							url={socials[2]} 
-							style={style} 
-							onMouseOver={() => setPlay3(true)} 
-							onMouseOut={() => setPlay3(false)} 
-						/>
-					</AnimateKeyframes>
-				</div>
-				<div style={mL}>
-					<AnimateKeyframes
-						{...props}
-						pause={!play4}
-						keyframes={[
-							"transform: scaleY(1) scaleX(1)",
-							"transform: scaleY(1.25) scaleX(1.25)",
-							"transform: scaleX(1) scaleY(1)",
-						]}
-					>
-						<SocialIcon 
-							url={socials[3]} 
-							style={style}
-							onMouseOver={() => setPlay4(true)} 
-							onMouseOut={() => setPlay4(false)} 
-						/>
-					</AnimateKeyframes>
+				<br />
+				<div className='container_row'>
+					<div style={mR}>
+						<AnimateKeyframes
+							{...props}
+							pause={!play3}
+							keyframes={[
+								"transform: rotate(0deg)",
+								"transform: rotate(360deg)",
+							]}
+						>
+							<SocialIcon 
+								url={socials[2]} 
+								style={style} 
+								onMouseOver={() => setPlay3(true)} 
+								onMouseOut={() => setPlay3(false)} 
+							/>
+						</AnimateKeyframes>
+					</div>
+					<div style={mL}>
+						<AnimateKeyframes
+							{...props}
+							pause={!play4}
+							keyframes={[
+								"transform: scaleY(1) scaleX(1)",
+								"transform: scaleY(1.25) scaleX(1.25)",
+								"transform: scaleX(1) scaleY(1)",
+							]}
+						>
+							<SocialIcon 
+								url={socials[3]} 
+								style={style}
+								onMouseOver={() => setPlay4(true)} 
+								onMouseOut={() => setPlay4(false)} 
+							/>
+						</AnimateKeyframes>
+					</div>
 				</div>
 			</div>
 		</div>
