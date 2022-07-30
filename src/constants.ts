9623a86046e7445e75e2b50828a23be451c10cd1
Olli Glorioso <olli.glorioso@avanio.com>
2022-07-30 20:12:04 +0300

    order change [build]

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const socials = [
-	"https://www.github.com/olliglorioso",
 	"https://www.linkedin.com/in/olliglorioso/",
+	"https://www.github.com/olliglorioso",
 	"mailto: olli.glorioso@gmail.com",
 	"https://www.kaggle.com/olliglorioso",
 ]
